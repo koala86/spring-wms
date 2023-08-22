1736adc539da37cd50e7d7a032a800d6dd54f5d8
koala86 <nanbiao66@gmail.com>
2023-08-23 06:48:18 +0900

    springboot initializr

---
+++ wms/src/main/java/com/koala/wms/WmsApplication.java
@@ -0,0 +1,14 @@
+package com.koala.wms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WmsApplication {
+
+	// test2
+	public static void main(String[] args) {
+		SpringApplication.run(WmsApplication.class, args);
+	}
+
+}
