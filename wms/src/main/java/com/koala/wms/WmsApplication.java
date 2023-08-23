d5579ec6a01d2366bee012d7080a748f13218b6c
koala86 <142942819+koala86@users.noreply.github.com>
2023-08-24 05:59:23 +0900

    Update WmsApplication.java

--- wms/src/main/java/com/koala/wms/WmsApplication.java
+++ wms/src/main/java/com/koala/wms/WmsApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WmsApplication {
 
-	// test2
 	public static void main(String[] args) {
 		SpringApplication.run(WmsApplication.class, args);
 	}
