cb5dd301e4bd443be6fc12a255cef8a8cec27efe
koala86 <142942819+koala86@users.noreply.github.com>
2025-12-07 18:07:08 +0900

    Exclude DataSourceAutoConfiguration from Spring Boot

--- wms/src/main/java/com/koala/wms/WmsApplication.java
+++ wms/src/main/java/com/koala/wms/WmsApplication.java
@@ -3,7 +3,7 @@ package com.koala.wms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class WmsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WmsApplication.class, args);
